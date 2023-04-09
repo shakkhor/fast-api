9d94baba50a8c5eccb7bc2416ca097f2a1b7564d
Abdullah Aziz Sharfuddin Shakkhor <abdullahshakkhor@gmail.com>
2023-04-09 11:30:00 +0600

    lil bit update

--- app/app.py
+++ app/app.py
@@ -1,5 +1,16 @@
 from fastapi import FastAPI
 import boto3
+from pydantic import BaseModel
+
+class Message(BaseModel):
+    message: str
+    queue: str
+
+
+class Queue(BaseModel):
+    name: str
+
+
 app = FastAPI()
 
 @app.get("/", tags=['ROOT'])
@@ -7,17 +18,17 @@ async def root() -> dict:
     return{"ping":"pong"}
 
 @app.post("/send", tags=['ROOT'])
-async def send():
+async def send(message: Message):
     sqs = boto3.client('sqs', endpoint_url='http://localhost:4566')
-    queue_url = 'http://localhost:4566/queue/test-queue'
-    response = sqs.send_message(QueueUrl=queue_url, MessageBody='chola muri peyaji beguni alur chop')
+    queue_url = 'http://localhost:4566/queue/'+message.queue
+    response = sqs.send_message(QueueUrl=queue_url, MessageBody=message.message)
     return response
 
 
-@app.get("/create", tags=['ROOT'])
-async def create():
+@app.post("/create", tags=['ROOT'])
+async def create(queue: Queue):
     sqs = boto3.client('sqs', endpoint_url='http://localhost:4566', )
-    queue_name = 'test-queue'
+    queue_name = queue.name
     response = sqs.create_queue(QueueName=queue_name)
     print(response)
     
@@ -36,8 +47,8 @@ async def get_queues(prefix=None):
     return queue_iter
 
 @app.get("/check-messages", tags=['ROOT'])
-async def check_messages():
-    queue_url = 'http://localhost:4566/queue/test-queue'
+async def check_messages(queue: str):
+    queue_url = 'http://localhost:4566/queue/'+queue
     sqs = boto3.client('sqs', endpoint_url='http://localhost:4566', )
     return sqs.receive_message(QueueUrl=queue_url)
 
